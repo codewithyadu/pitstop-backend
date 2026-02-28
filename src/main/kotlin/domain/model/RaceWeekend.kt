bb52ba490bec06eefac3a07c5e61d54a8ef2e2d8
yadvendukumar <codewithyadu@gmail.com>
2026-02-28 16:35:10 +0530

    chore(domain): add RaceWeekend domain model

---
+++ src/main/kotlin/domain/model/RaceWeekend.kt
@@ -0,0 +1,35 @@
+package com.pitstop.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RaceWeekend(
+    val round: Int,
+    val totalRounds: Int,
+    val raceName: String,
+    val circuitId: String,
+    val circuitName: String,
+    val country: String,
+    val locality: String,
+    val lat: String,
+    val long: String,
+    val sessions: List<Session>,
+    val isSprintWeekend: Boolean
+)
+
+@Serializable
+data class Session(
+    val type: SessionType,
+    val date: String,
+    val time: String
+)
+
+enum class SessionType {
+    PRACTISE_1,
+    PRACTISE_2,
+    PRACTISE_3,
+    SPRINT_QUALIFYING,
+    SPRINT,
+    QUALIFYING,
+    RACE
+}
